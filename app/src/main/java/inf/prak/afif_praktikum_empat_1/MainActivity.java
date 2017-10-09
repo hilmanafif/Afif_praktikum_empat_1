19a1a5a9fb4ab477b8f5fd34a270c9785af31ba3
hilmanafif <24accord@gmail.com>
2017-10-09 17:34:58 +0700

    Menambahkan activity dan merubah teks assalmuálaikum pada activity_main.xml

--- app/src/main/java/inf/prak/afif_praktikum_empat_1/MainActivity.java
+++ app/src/main/java/inf/prak/afif_praktikum_empat_1/MainActivity.java
@@ -2,12 +2,18 @@ package inf.prak.afif_praktikum_empat_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //ini app saya
+    
+    TextView teks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        teks = (TextView) findViewById(R.id.teks)
     }
 }
